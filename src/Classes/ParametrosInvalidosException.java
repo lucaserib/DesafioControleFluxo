4cd813134e435fd1586520bad99c5ab1c1f95f3a
Lucas <lucasemanuelribeiro@hotmail.com>
2024-05-22 22:32:26 -0300

    Finalizando Desafio

--- src/Classes/ParametrosInvalidosException.java
+++ src/Classes/ParametrosInvalidosException.java
@@ -1,4 +1,7 @@
 package Classes;
 
-public class ParametrosInvalidosException {
+public class ParametrosInvalidosException extends Exception {
+    public  ParametrosInvalidosException(String message) {
+        super(message);
+    }
 }
